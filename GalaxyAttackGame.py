160823f67b7805261dd98b7874efa7bcaf0164c9
Moroz Kostimo <kostyamoroz50@gmail.com>
2020-12-12 18:48:24 +0200

    Добавлены движущиеся мобы и персонаж с управлением

--- GalaxyAttackGame.py
+++ GalaxyAttackGame.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
-WIDTH = 400
+WIDTH = 480
 HEIGHT = 600
-FPS = 24
+FPS = 60
 
 # Цвета (R, G, B)
 BLACK = (0, 0, 0)
@@ -14,18 +15,55 @@ BLUE = (0, 0, 255)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,50))
+        self.image = pygame.Surface((50,40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH//2
+        self.rect.bottom = HEIGHT-25
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.speedx = -8
+        if keys[pygame.K_d]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH//2,  HEIGHT//2)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedx = random.randrange(-2, 2)
+        self.speedy = random.randrange(1, 8)
+
     def update(self):
-        self.rect.x += 7
-        if self.rect.x > WIDTH:
-            self.rect.right = 0
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedx = random.randrange(-2, 2)
+            self.speedy = random.randrange(1, 8)
 
 all_sprites = pygame.sprite.Group()
+
 player = Player()
 all_sprites.add(player)
+
+for _ in range(8):
+    m = Mob()
+    all_sprites.add(m)
 
 #игра и окно
 pygame.init()
@@ -43,10 +81,14 @@ while GAME:
 
     #обновление спрайтов
     all_sprites.update()
+
     #рендеринг
-    screen.fill((0, 255, 255))
+    screen.fill((BLACK))
     all_sprites.draw(screen)
+
     #обновление дисплея
-    pygame.display.update()
+    pygame.display.flip()
+
+pygame.quit()
 
 
