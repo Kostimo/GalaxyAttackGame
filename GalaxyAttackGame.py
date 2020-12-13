59305acf307a3f1e8c1ac6f334a2305f4346dab8
Moroz Kostimo <kostyamoroz50@gmail.com>
2020-12-13 08:49:19 +0200

    Стрельба.Обработка столкновений игрока,мобов,пуль.

--- GalaxyAttackGame.py
+++ GalaxyAttackGame.py
@@ -12,6 +12,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# Класс игрока
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -22,6 +23,11 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT-25
         self.speedx = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
     def update(self):
         self.speedx = 0
         keys = pygame.key.get_pressed()
@@ -30,12 +36,13 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_d]:
             self.speedx = 8
         self.rect.x += self.speedx
-
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        
 
+# Класс моба
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -56,7 +63,26 @@ class Mob(pygame.sprite.Sprite):
             self.speedx = random.randrange(-2, 2)
             self.speedy = random.randrange(1, 8)
 
+# Класс пули
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+        
+    def update(self):
+        self.rect.y += self.speedy
+        # Убить, если он заходит за верхнюю часть экрана
+        if self.rect.bottom < 0:
+            self.kill()
+
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 
 player = Player()
 all_sprites.add(player)
@@ -64,29 +90,45 @@ all_sprites.add(player)
 for _ in range(8):
     m = Mob()
     all_sprites.add(m)
+    mobs.add(m)
 
-#игра и окно
+# Игра и окно
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("SHOOTER!")
+pygame.display.set_caption("!!!GALAXY ATTACK!!!")
 clock = pygame.time.Clock()
 
-#цикл игры
+# Цикл игры
 GAME = True
 while GAME:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GAME = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
-    #обновление спрайтов
+    # Проверка, не ударил ли моб игрока
+    hit_with_player = pygame.sprite.spritecollide(player, mobs, True)
+    if hit_with_player:
+        GAME = False
+
+    # Проверка столкновений пуль и мобов
+    hits_with_bullets = pygame.sprite.groupcollide(bullets, mobs, True, True)
+    for hit in hits_with_bullets:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    # Обновление спрайтов
     all_sprites.update()
 
-    #рендеринг
+    # Рендеринг
     screen.fill((BLACK))
     all_sprites.draw(screen)
 
-    #обновление дисплея
+    # Обновление дисплея
     pygame.display.flip()
 
 pygame.quit()
